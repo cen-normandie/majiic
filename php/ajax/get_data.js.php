cd159169deada1e31865ea98d8b21577b1acdc0b
Benno <b.perceval@superceval.fr>
2022-08-03 17:44:00 +0200

    a

--- php/ajax/get_data.js.php
+++ php/ajax/get_data.js.php
@@ -2,7 +2,6 @@
 session_start();
 include '../properties.php';
 
-
 //connexion a la BD
 $dbconn = pg_connect("hostaddr=$DBHOST port=$PORT dbname=$DBNAME user=$LOGIN password=$PASS")
 or die ('Connexion impossible :'. pg_last_error());
