4fd1397164e81d2301d8206a8c32151f1678ba57
Benno <b.perceval@superceval.fr>
2022-08-17 16:26:48 +0200

    id_action

--- js/suivi_projet.js
+++ js/suivi_projet.js
@@ -172,30 +172,31 @@ function update_view_projet(projets_json) {
         <div class="d-flex flex-column w-100 p-2 m-2 border border-success rounded">
             <div class="d-flex w-100 gx-1 align-items-center justify-content-between ">
                 <div class="col-xs-1 text-truncate">
-                    <label id="id_action_${actions}" class="p-1 col-form-label text-secondary">${actions_f[actions].id_action}</label>
+                    <label id="id_action_${actions_f[actions].id_action}" id_action="${actions_f[actions].id_action}" class="p-1 col-form-label text-secondary">${actions_f[actions].id_action}</label>
                 </div>
                 <div class="col-xs-3 text-truncate">
-                    <span type="text" id="nom_action_${actions}" value="">${actions_f[actions].code_action}</span>
+                    <span type="text" id="nom_action_${actions_f[actions].id_action}" value="">${actions_f[actions].code_action}</span>
                 </div>
                 <div class="col-xs-3">
-                    <span type="text"  id="financeurs_action_${actions}" value="" >${actions_f[actions].financements}</span>
+                    <span type="text"  id="financeurs_action_${actions_f[actions].id_action}" value="" >${actions_f[actions].financements}</span>
                 </div>
                 <div class="col-xs-2">
-                    <span type="text"  id="site_action_${actions}" value="" >${actions_f[actions].site}</span>
+                    <span type="text"  id="site_action_${actions_f[actions].id_action}" value="" >${actions_f[actions].site}</span>
                 </div>
                 <div class="col-xs-1">
-                    <span type="text"  id="heures_action_${actions}" value="" disabled>${actions_f[actions].nb_h}</span>
+                    <span type="text"  id="heures_action_${actions_f[actions].id_action}" value="" disabled>${actions_f[actions].nb_h}</span>
                 </div>
                 <div class="col-xs-1">
-                    <button id="add_p_action_${actions}" class=" bg-light border-0 text-success fs-6 m-1 px-1" ><i class="fas fa-user-plus"></i></button>
-                    <button id="del_action_${actions}" class=" bg-light border-0 text-danger fs-6 m-1 px-1" ><i class="fas fa-trash-alt"></i></button>
+                    <button id="add_p_action_${actions_f[actions].id_action}" id_action="${actions_f[actions].id_action}" class=" bg-light border-0 text-success fs-6 m-1 px-1" ><i class="fas fa-user-plus"></i></button>
+                    <button id="del_action_${actions_f[actions].id_action}" id_action="${actions_f[actions].id_action}" class=" bg-light border-0 text-danger fs-6 m-1 px-1" ><i class="fas fa-trash-alt"></i></button>
                 </div>
             </div>
-            <div class="d-flex flex-wrap justify-content-end align-items-center id="list_personnes_action_${actions}">
+            <div class="d-flex flex-wrap justify-content-end align-items-center id="ist_personnes_action_${actions_f[actions].id_action}">
                 ${badges_}
             </div>
         </div>`
         );
+        add_events_actions ();
         c_actions++;
     }
 }
@@ -290,12 +291,15 @@ function get_action_content () {
             str_f=str_f+fx_+"_"+fx_p;
             i++;
 	    });
+
+        //add Ajax function to have valid id_action
+
         document.getElementById('list_actions').insertAdjacentHTML("beforeend", 
         `
         <div class="d-flex flex-column w-100 p-2 m-2 border border-success rounded">
             <div class="d-flex w-100 gx-1 align-items-center justify-content-between ">
                 <div class="col-xs-1 text-truncate">
-                    <label id="id_action_${c_actions}" class="p-1 col-form-label text-secondary">${c_actions}</label>
+                    <label id="id_action_${c_actions}" id_action="" class="p-1 col-form-label text-secondary">${c_actions}</label>
                 </div>
                 <div class="col-xs-3 text-truncate">
                     <span type="text" id="nom_action_${c_actions}" value="">${action}</span>
@@ -310,34 +314,40 @@ function get_action_content () {
                     <span type="text"  id="heures_action_${c_actions}" value="" disabled>${heures}</span>
                 </div>
                 <div class="col-xs-1">
-                    <button id="add_p_action_${c_actions}" class=" bg-light border-0 text-success fs-6 m-1 px-1" ><i class="fas fa-user-plus"></i></button>
-                    <button id="del_action_${c_actions}" class=" bg-light border-0 text-danger fs-6 m-1 px-1" ><i class="fas fa-trash-alt"></i></button>
+                    <button id="add_p_action_${c_actions}" id_action="" class=" bg-light border-0 text-success fs-6 m-1 px-1" ><i class="fas fa-user-plus"></i></button>
+                    <button id="del_action_${c_actions}" id_action="" class=" bg-light border-0 text-danger fs-6 m-1 px-1" ><i class="fas fa-trash-alt"></i></button>
                 </div>
             </div>
-            <div class="d-flex flex-wrap justify-content-end align-items-center id="list_personnes_action_${c_actions}">
+            <div class="d-flex flex-wrap justify-content-end align-items-center" id="list_personnes_action_${c_actions}">
             </div>
         </div>`);
     }
 document.getElementById("add_action").addEventListener("click", function() {
     get_action_content();
-    add_events_actions (); //buttons add user, delete, modal..
+    add_events_actions(); //buttons add user, delete, modal..
     c_actions++;
 });
 
 function add_events_actions () {
     const elements1 = document.querySelectorAll(`[id^="add_p"]`);
     elements1.forEach(element => {
-        console.log(element);
         element.addEventListener("click", function() {
             //c_action = projets_f.id
-            document.getElementById("id_action_update").insertAdjacentHTML("beforeend", element.getAttribute('id').replace('add_p_action_', '') );
+            document.getElementById("id_action_update").textContent=element.getAttribute('id').replace('add_p_action_', '');
             ModalAddPersonne.show(element.getAttribute('id'));
         });
     });
 
 }
 
-
+document.getElementById("update_action_personne").addEventListener("click", function() {
+    if (!!document.getElementById("input_personnes").value) {
+        console.log("list_personnes_action_"+document.getElementById("id_action_update").textContent);
+        let s = '<div id=""><span class="badge mt-1 bg-success text-light">'+document.getElementById("input_personnes").value+'<i id="" class="ps-1 fas fa-window-close"></i></span></div>';
+        document.getElementById("list_personnes_action_"+document.getElementById("id_action_update").textContent ).insertAdjacentElement("beforeend", s)
+    }
+    
+});
 
 //////////////////////////////////////////////////////
 //Gestion des dom et evenement pour le graphique
