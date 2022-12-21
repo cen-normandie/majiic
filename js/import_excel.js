c3034e3c9be1228d4c15b1776e8217d1bd63a211
Benno <b.perceval@superceval.fr>
2022-12-21 10:37:56 +0100

    load step by step

--- js/import_excel.js
+++ js/import_excel.js
@@ -18,6 +18,7 @@ document.getElementById("load_file").addEventListener("click", function() {
     if( document.getElementById("input_file").files.length == 0 ){
         alert("Aucun fichier selectionné !");
     } else {
+        change_load("Chargement du fichier ...");
         if ( validate_extension() ) {
             let active_file = document.getElementById("input_file").files[0];
             let fd = new FormData();
@@ -29,13 +30,25 @@ document.getElementById("load_file").addEventListener("click", function() {
                 processData : false,
                 contentType : false,
                 async    : true,
-                error    : function(request, error) { alert("Erreur : responseText: "+request.responseText);},
+                error    : function(request, error) { 
+                    alert("Erreur : responseText: "+request.responseText);
+                    change_load();
+                },
                 success  : function(data) {
-                        console.log(data);
-                        }
+                    change_load();
+                    console.log(data);
+                    if (data !== 'impossible de copier le fichier') {
+                        console.log(data+" next ");
+                        read_excel_file_by_line();
+                    }
+                }
                 });
         } else {
             alert("Extension de fichier non valide !");
         }
     }
 });
+
+function read_excel_file_by_line() {
+    change_load("Lecture des lignes du fichier...");
+}
---
+++ php/read_lines_excel.php
@@ -0,0 +1,60 @@
+<?php
+include 'properties.php';
+require '../vendor/autoload.php';
+define('ROOT_DIR', dirname(__FILE__).DIRECTORY_SEPARATOR);
+
+$path = ROOT_DIR;
+$path_doc_files = $path.DIRECTORY_SEPARATOR.'files';
+$path_parts = pathinfo($_FILES['file']['name']);
+$extension = ".".$path_parts['extension'];
+$file_name_tmp = $path_parts['filename'];
+
+if( isset($_FILES["file"]))
+{
+    $file_name = $file_name_tmp.'_'.date("d-m-Y_h_i_s");
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $path_doc_files.DIRECTORY_SEPARATOR.$file_name.$extension )) {
+        echo $file_name;
+        $inputFileName = $path_doc_files.DIRECTORY_SEPARATOR.$file_name.$extension;
+        /**  Identify the type of $inputFileName  **/
+        $inputFileType = \PhpOffice\PhpSpreadsheet\IOFactory::identify($inputFileName);
+        /**  Create a new Reader of the type that has been identified  **/
+        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
+        /**  Load $inputFileName to a Spreadsheet Object  **/
+        $sheet = $reader->load($inputFileName);
+        $highestRow = $sheet->getActiveSheet()->getHighestRow();
+        echo '|||||||  '.$highestRow.' ||||||  ';
+        $arr_columnname = ['id','objet','start','end','id_projet','id_action','nom_action','nom_projet','lieu','commentaire','salissure','panier','date_saisie','date_saisie_salissure','color','personne'];
+        //ignore first line
+        for ($row = 0; $row <= $highestRow; $row++) {
+            $row_in_file = $row + 1;      
+        }
+        /* //connexion a la BD
+        $dbconn = pg_connect("hostaddr=$DBHOST port=$PORT dbname=$DBNAME user=$LOGIN password=$PASS")
+        or die ('Connexion impossible :'. pg_last_error());
+        $result = pg_prepare($dbconn, "up", "
+            INSERT INTO $files (id_projet, file_name) VALUES ($1,$2);"
+        );
+        $return_execute = pg_execute($dbconn, "up",array($_POST["id_projet"], basename($file_name).$extension ));
+        //ferme la connexion a la BD
+        pg_close($dbconn); */
+
+
+    } else {
+        //echo "Error";
+        echo "impossible de copier le fichier";
+    }
+}
+echo $file_name;
+
+
+
+
+
+
+
+?>
+
+
+
+
+
