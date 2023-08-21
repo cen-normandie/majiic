b40b311f757c7b65f0d6ba4936c2361ecbd5a9d7
Benno <b.perceval@superceval.fr>
2023-08-21 11:46:50 +0200

    Update test_mail.php

--- php/test_mail.php
+++ php/test_mail.php
@@ -49,7 +49,7 @@ $message_html .= "</html>";
 
 
 $headers = "MIME-Version: 1.0\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+$headers .= "Content-type: text/html; charset=utf-8\n";
 $headers .= 'From:' . $emetteur;
 
 if (mail($destinataire, $sujet, $message_html, $headers))
