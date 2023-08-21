063f815887e7f7643f39a6fa19f4259341aed01f
Benno <b.perceval@superceval.fr>
2023-08-21 11:30:41 +0200

    Update test_mail.php

--- php/test_mail.php
+++ php/test_mail.php
@@ -1,6 +1,6 @@
 <?PHP
 $emetteur = 'test@cenwms.xyz';
-$destinataire = 'b.perceval@superceval.fr';
+$destinataire = 'b.perceval@cen-normandie.fr';
 
 $sujet = "Test de la fonction PHP mail";
 $message = "Ceci est un message de test";
