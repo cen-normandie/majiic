c679b0607a52f6bdf771665d1717cc5d792b56a9
Benno <b.perceval@superceval.fr>
2023-08-25 09:44:13 +0200

    Update copy_gpkg_nc.php

--- export/copy_gpkg_nc.php
+++ export/copy_gpkg_nc.php
@@ -1,6 +1,6 @@
 <?php
 
 echo copy("/var/www/html/nextcloud/data/8ACA0A1C-6E6E-4912-8511-DB8A02F1CA67/files/QGIS/Saisie_Libre/saisie_libre.gpkg",
-"/var/www/html/majiic/export/8ACA0A1C-6E6E-4912-8511-DB8A02F1CA67/saisie_libre.gpkg");
+"/var/www/html/majiic/export/saisie_libre.gpkg");
 
 ?>
