3996db3ba0f0d18bc004e5a0172903e62eb09ad8
Benno <b.perceval@superceval.fr>
2023-08-25 11:51:53 +0200

    Update copy_gpkg_nc.php

--- export/copy_gpkg_nc.php
+++ export/copy_gpkg_nc.php
@@ -6,7 +6,7 @@ $ldaphost="192.168.0.211";
 $ldapconn=ldap_connect($ldaphost);
 //$filter = "(&(mail=$courriel)(memberof=CN=PROGECEN_SALARIE,CN=Users,DC=CSNHN,DC=LOCAL))";
 $SEARCH_STRING = 'NextcloudUsers';
-$ldaptree    = "(|(dc=$SEARCH_STRING)(o=$SEARCH_STRING)(ou=$SEARCH_STRING) (cn=$SEARCH_STRING)(uid=$SEARCH_STRING)(mail=$SEARCH_STRING))";
+$ldaptree    = "(cn=NextcloudUsers)";
 if($ldapconn) {
 	echo "Connect success<br>";
     ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
