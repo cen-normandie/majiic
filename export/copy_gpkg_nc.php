36f17056455302ebcab4d69a1469ae1c4ed8aaf7
Benno <b.perceval@superceval.fr>
2023-08-25 11:20:00 +0200

    Update copy_gpkg_nc.php

--- export/copy_gpkg_nc.php
+++ export/copy_gpkg_nc.php
@@ -4,30 +4,30 @@
 //For testing the AD server is work or not
 $ldaphost="192.168.0.211";
 $ldapconn=ldap_connect($ldaphost);
-if($ldapconn)
+if($ldapconn) {
 	echo "Connect success<br>";
-else
-	echo "Connect Failure";
-ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
-ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
-//Reference：http://php.net/manual/en/function.ldap-bind.php
-if ($ldapconn) {
-    // binding to ldap server
-    $ldapbind = ldap_bind($ldapconn, "CSNHN\Administrateur", "CENN2021");
-    // verify binding
-    if ($ldapbind) {
-        echo "LDAP bind successful...";
-        $ldap_base_dn = "DC=CSNHN,DC=LOCAL";
-        $filter = "(&(memberof=CN=PROGECEN_SALARIE,CN=Users,DC=CSNHN,DC=LOCAL))";
-        $justthese = array("mail", "samaccountname", "sn", "givenname", "cn", "memberof");
-        $result = ldap_search($ldapconn, $ldap_base_dn, $filter, $justthese);
-        $result_salarie = ldap_get_entries($ldapconn, $result);
-        echo in_array("CN=NextcloudUsers,CN=Users,DC=CSNHN,DC=LOCAL", $result_salarie[0]["memberof"]);
-        
-    } else {
-        echo "LDAP bind failed...";
-    }
+    ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+    ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
+        // binding to ldap server
+        $ldapbind = ldap_bind($ldapconn, "CSNHN\Administrateur", "CENN2021");
+        // verify binding
+        if ($ldapbind) {
+            echo "LDAP bind successful...";
+            $ldap_base_dn = "DC=CSNHN,DC=LOCAL";
+            $filter = "(&(memberof=CN=PROGECEN_SALARIE,CN=Users,DC=CSNHN,DC=LOCAL))";
+            $justthese = array("mail", "samaccountname", "sn", "givenname", "cn", "memberof");
+            $result = ldap_search($ldapconn, $ldap_base_dn, $filter, $justthese);
+            $result_salarie = ldap_get_entries($ldapconn, $result);
+            echo in_array("CN=NextcloudUsers,CN=Users,DC=CSNHN,DC=LOCAL", $result_salarie[0]["memberof"]);
+            
+        } else {
+            echo "LDAP bind failed...";
+        }
 }
+else {
+	echo "Connect Failure";
+}
+
 
 
 //echo copy("/var/www/html/nextcloud/data/8ACA0A1C-6E6E-4912-8511-DB8A02F1CA67/files/QGIS/Saisie_Libre/saisie_libre.gpkg",
