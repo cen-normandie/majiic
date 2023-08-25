55852fa2709030698b56f27415e84b14514f1b05
Benno <b.perceval@superceval.fr>
2023-08-25 10:13:23 +0200

    up file

--- export/copy_gpkg_nc.php
+++ export/copy_gpkg_nc.php
@@ -3,4 +3,6 @@
 echo copy("/var/www/html/nextcloud/data/8ACA0A1C-6E6E-4912-8511-DB8A02F1CA67/files/QGIS/Saisie_Libre/saisie_libre.gpkg",
 "/var/www/html/majiic/export/saisie_libre.gpkg");
 
+exec('ogr2ogr -f PostgreSQL "PG:host=192.168.0.218 user=postgres password=postgres dbname=test" saisie_libre.gpkg');
+
 ?>
