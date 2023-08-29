7bacba4b1148d35dfbb27da7d033b8fa6ce0771e
Benno <b.perceval@superceval.fr>
2023-08-29 18:53:41 +0200

    Update copy_gpkg_nc.php

--- export/copy_gpkg_nc.php
+++ export/copy_gpkg_nc.php
@@ -4,40 +4,25 @@
 //For testing the AD server is work or not
 $ldaphost="192.168.0.211";
 $ldapconn=ldap_connect($ldaphost);
-//$filter = "(&(mail=$courriel)(memberof=CN=PROGECEN_SALARIE,CN=Users,DC=CSNHN,DC=LOCAL))";
-$SEARCH_STRING = 'NextcloudUsers';
-$ldaptree    = "(&(objectClass=user)(memberof=CN=PROGECEN_SALARIE,CN=Users,DC=CSNHN,DC=LOCAL))";
+
 if($ldapconn) {
 	echo "Connect success<br>";
     ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
     ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
     // binding to ldap server
-    $ldapbind = ldap_bind($ldapconn, "CSNHN\Administrateur", "CENN2021");
+    $ldapbind = ldap_bind($ldapconn, 'CSNHN\Administrateur', 'CENN2021');
     // verify binding
     if ($ldapbind) {
-        $result = ldap_search($ldapconn,$ldaptree, "(cn=*)") or die ("Error in search query: ".ldap_error($ldapconn));
-        $data = ldap_get_entries($ldapconn, $result);
-        
-        // SHOW ALL DATA
-        echo '<h1>Dump all data</h1><pre>';
-        print_r($data);    
-        echo '</pre>';
-        
-        
-        // iterate over array and print data for each entry
-        echo '<h1>Show me the users</h1>';
-        for ($i=0; $i<$data["count"]; $i++) {
-            //echo "dn is: ". $data[$i]["dn"] ."<br />";
-            echo "User: ". $data[$i]["cn"][0] ."<br />";
-            if(isset($data[$i]["mail"][0])) {
-                echo "Email: ". $data[$i]["mail"][0] ."<br /><br />";
-            } else {
-                echo "Email: None<br /><br />";
-            }
-        }
-        // print number of entries found
-        echo "Number of entries found: " . ldap_count_entries($ldapconn, $result);
-            
+        echo "Bind success<br>";
+        $filter="(memberof=CN=NextcloudUsers,OU=Groupes,DC=CSNHN,DC=LOCAL)";
+        $search=ldap_search($ldapconn, "DC=CSNHN,DC=LOCAL", $filter) or die ("Error in search query: ".ldap_error($ldapconn));
+        $result= ldap_get_entries($ldapconn, $search);
+        for ($i=0; $i<$result["count"]; $i++)
+        {
+            echo $result[$i]["description"][0];
+            echo ' || '.$result[$i]["mail"][0];
+            echo ' || '.$result[$i]["cn"][0].'<br>';
+        }   
     } else {
         echo "LDAP bind failed...";
     }
