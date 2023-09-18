cc1661ad11d20c263d75cc436b9f2dbaf0e8abbc
Benno <b.perceval@superceval.fr>
2023-09-18 16:21:28 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -35,7 +36,7 @@ ini_set ('display_errors', 1);
 ini_set ('display_startup_errors', 1);
 error_reporting (E_ALL);
 echo $truc;
-session_start();
+
 
 include 'php/properties.php';
 
