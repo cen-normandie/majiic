eba2a12496f620acd7a36b4ef2c4f8872ad6fc32
Benno <b.perceval@superceval.fr>
2024-02-20 11:48:55 +0100

    Update gestion.js

--- js/gestion.js
+++ js/gestion.js
@@ -566,7 +566,7 @@ function save_bdd_ddg(id_doc_, lien) {
         fd.append('n_type_doc_gestion', $("#n_type_doc_gestion").val());
         fd.append('n_gestion_date_start', $("#n_gestion_date_start").val());
         fd.append('n_gestion_date_end', $("#n_gestion_date_end").val());
-        let default_date = ($("#n_gestion_date_maj").val()) ?? ($("#n_gestion_date_start").val()) ;
+        let default_date = ( ($("#n_gestion_date_maj").val()) ?  $("#n_gestion_date_maj").val() : ($("#n_gestion_date_start").val())  );
         fd.append('n_gestion_date_maj', default_date );
         fd.append('n_gestion_auteurs', $("#n_gestion_auteurs").val());
         fd.append('n_gestion_commentaire', $("#n_gestion_commentaire").val());
