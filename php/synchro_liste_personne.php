20273861564cd709ad949b423a30e445d3d85fa0
Benno <b.perceval@superceval.fr>
2024-08-07 11:45:48 +0200

    Update synchro_liste_personne.php

--- php/synchro_liste_personne.php
+++ php/synchro_liste_personne.php
@@ -12,7 +12,7 @@ include 'properties.php';
             // verify binding
             if ($ldapbind) {
                 echo "LDAP bind successful...";
-                //POUR AVOIR UN TABLEAU AVEC LA LISTE DES SALARIES
+                //genere la liste des utilisateurs progecen depuis l'ad (groupe : progecen_user)
                 $filter="(cn=progecen_user)";
                 $result=ldap_search($ldapconn, "DC=CSNHN,DC=LOCAL", $filter);
                 $entries= ldap_get_entries($ldapconn, $result);
@@ -36,6 +36,35 @@ include 'properties.php';
                 
                 $result = pg_prepare($dbconn, "sql", "INSERT INTO progecen_copy.liste_personne_ (personne) VALUES ( $1 );");
                 foreach($list_personne as $personne) {
+                    $result = pg_execute($dbconn, "sql", array($personne)) or die ('Connexion impossible :'. pg_last_error());
+                    echo $personne;
+                }
+                pg_close($dbconn);
+
+                //genere la liste des responsable de projet depuis l'ad (groupe : progecen_resp_projet)
+                $filter="(cn=progecen_resp_projet)";
+                $result=ldap_search($ldapconn, "DC=CSNHN,DC=LOCAL", $filter);
+                $entries= ldap_get_entries($ldapconn, $result);
+                $groups = $entries[0]["member"];
+                $list_resp_projet = array();
+
+                foreach($groups as $group) {
+                    if (str_contains($group, "CN=")) {
+                        $name_a = explode("CN=", $group)[1];
+                        $name_ = explode(",OU", $name_a)[0];
+                        array_push($list_resp_projet, $name_);
+                    }
+                    sort($list_resp_projet);
+                }
+                
+                $dbconn = pg_connect("hostaddr=$DBHOST port=$PORT dbname=$DBNAME user=$LOGIN password=$PASS") or die ('Connexion impossible :'. pg_last_error());
+                $del = pg_prepare($dbconn, "sql_del", "DELETE FROM progecen_copy.liste_resp_projet ");
+                $del = pg_execute($dbconn, "sql_del", array());
+                $seq = pg_prepare($dbconn, "sql_seq", "ALTER SEQUENCE progecen_copy.liste_resp_projet_id_seq RESTART WITH 1;");
+                $seq = pg_execute($dbconn, "sql_seq", array());
+                
+                $result = pg_prepare($dbconn, "sql", "INSERT INTO progecen_copy.liste_resp_projet (personne) VALUES ( $1 );");
+                foreach($list_resp_projet as $personne) {
                     $result = pg_execute($dbconn, "sql", array($personne)) or die ('Connexion impossible :'. pg_last_error());
                     echo $personne;
                 }
