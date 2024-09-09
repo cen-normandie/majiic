9faeaf137bb2ed50292452b0acb0257adefbc4dd
Benno <b.perceval@superceval.fr>
2024-09-09 10:15:43 +0200

    sensibilisation

---
+++ hack/hackend.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+<title>Hack</title>
+<style type="text/css">
+      body { text-align: center; padding: 150px; background-image: url("tmp.jpg"); }
+      h1 { font-size: 40px; }
+      body { font: 20px Helvetica, sans-serif; color: #333; }
+      #article { display: block; text-align: center; width: 500px; margin: 0 auto; }
+      a { color: #dc8100; text-decoration: none; }
+      a:hover { color: #333; text-decoration: none; }
+      .classic {
+      background-color:#fff;
+      }
+      .cen {
+          color: #9d0061
+      }
+      .blink {
+        animation: blink 5s infinite;
+      }
+      @-moz-keyframes blink { 
+        0%  { opacity:0; }
+        20% { opacity:0; }
+        21% { opacity:1; }
+        40% { opacity:1; }
+        41% { opacity:0; } 
+        60% { opacity:1; }
+        61% { opacity:0; } 
+        80% { opacity:0; } 
+        100%{ opacity:0; }
+      }
+    </style>
+</head>
+<?php
+include '../php/properties.php';
+$str = '';
+if( isset($_SERVER['HTTP_CLIENT_IP']) ) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} else if ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else if (isset($_SERVER['REMOTE_ADDR'])) {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+
+    $dbconn = pg_connect("hostaddr=$DBHOST port=$PORT dbname=$DBNAME user=$LOGIN password=$PASS") or die ('Connexion impossible :'. pg_last_error());
+                $result = pg_prepare($dbconn, "sql", "INSERT INTO $hack_ (ip) VALUES ( $1 );");
+                $result = pg_execute($dbconn, "sql", array($ip)) or die ('Connexion impossible :'. pg_last_error());
+    pg_close($dbconn);
+
+?>
+<body>
+
+<div id="article">
+<h2><span class="cen blink"><bold>Thank You ! <?php echo ' '.$ip ;?></bold></span></h2>
+</div>
+</html>
