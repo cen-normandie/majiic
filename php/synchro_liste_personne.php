37904319322dfebde17eb737f2f14a8fa69a3df4
Benno <b.perceval@superceval.fr>
2024-09-11 16:05:45 +0200

    Update synchro_liste_personne.php

--- php/synchro_liste_personne.php
+++ php/synchro_liste_personne.php
@@ -112,6 +112,7 @@ include 'properties.php';
                 echo "</br>Hack</br>";
 
                 foreach($groups as $group) {
+                    echo $group;
                     if (str_contains($group, "CN=")) {
                         $name_a = explode("CN=", $group)[1];
                         $name_ = explode(",OU", $name_a)[0];
