2fed13f28e4e056e2f51ea105408a0c92313004d
Benno <b.perceval@superceval.fr>
2024-09-13 11:59:29 +0200

    Update hackend.php

--- hack/hackend.php
+++ hack/hackend.php
@@ -35,7 +35,7 @@
 <?php
 include '../php/properties.php';
 
-$str = '';
+$ip = '';
 if( isset($_SERVER['HTTP_CLIENT_IP']) ) {
     $ip = $_SERVER['HTTP_CLIENT_IP'];
 } else if ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
