7148861db9ac8ef37277deb2d4bb43d180d3730b
Benno <b.perceval@superceval.fr>
2025-02-25 16:21:56 +0100

    Update load_events_calendar.js.php

--- php/ajax/analytique/load_events_calendar.js.php
+++ php/ajax/analytique/load_events_calendar.js.php
@@ -30,7 +30,7 @@ SELECT
 FROM $progecen_temps e 
 LEFT JOIN $progecen_projets p on e.e_id_projet = p.id_projet::text 
 WHERE e.e_personne = $1
-AND e.e_start > (now()::date - (INTERVAL '1 year' + INTERVAL '1 month'))
+AND e.e_start > (now()::date - (INTERVAL '1 year' + INTERVAL '3 month'))
 "
 //LEFT JOIN $progecen_actions a on e.e_id_action = a.id::text
 );
