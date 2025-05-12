b67e0777a557aee0ba68ca3c2ebf3bb03025ac2c
Benno <b.perceval@superceval.fr>
2025-05-12 10:14:38 +0200

    Update projets.ajax.js.php

--- php/ajax/projets/projets.ajax.js.php
+++ php/ajax/projets/projets.ajax.js.php
@@ -9,6 +9,8 @@ $result = pg_prepare($dbconn, "sql",
 
 
 //array_to_json(array_agg(f)) As features
+// 12-05-2025 : sum(a.nb_h_previ) as previ,
+//a.nb_h_previ as previ
 "
 with 
 json_actions as 
@@ -21,7 +23,7 @@ json_actions as
          a.financements,
          a.site,
          a.personnes as personne_action,
-         sum(a.nb_h_previ) as previ,
+         a.nb_h_previ as previ,
          coalesce(sum(t.realise), 0) as realise,
          p2.color,
          p2.financement_default as financement
@@ -58,58 +60,14 @@ SELECT
  p.files,
  j.json_actions
  FROM $progecen_projets p left join json_actions j on p.id_projet = j.id_projet
-  WHERE p.etat <> 'Réalisé'  
-  AND (date_fin > to_date( $1::text||'0101', 'YYYYMMDD') OR (date_fin > to_date( '2024'::text||'0101', 'YYYYMMDD') ) )
+  WHERE  (date_fin > to_date( $1::text||'0101', 'YYYYMMDD') OR (date_fin > to_date( '2024'::text||'0101', 'YYYYMMDD') ) )
    )
 SELECT json_agg(t) FROM t;
 "
 );
-// OLD QUERY LONGER
-/* 
-WITH t as (
-  SELECT 
-  p.id_projet as id, 
-  p.nom_projet as name, 
-  'projet' as tablename,
-  p.territoire, 
-  p.type_projet, 
-  p.date_debut, 
-  p.date_fin, 
-  p.etat, 
-  p.responsable_projet, 
-  p.multi_site, 
-  p.nombre_financeur, 
-  p.commentaire_projet, 
-  p.annee_saisie, 
-  p.date_demande_solde, 
-  p.date_butoir_dossier, 
-  p.tags,
-  p.color,
-  p.sites,
-  p.files,
-  (select json_agg(acts) FROM 
-   	(SELECT
-          a.id_action as id_action,
-          a.id_projet,
-          a.code_action,
-          a.financements,
-          a.site,
-          a.personnes as personne_action,
-          sum(a.nb_h_previ) as previ,
-          coalesce(sum(t.realise), 0) as realise,
-          p.color
-	  FROM $progecen_projets p2 left join 
-          $progecen_actions a on p2.id_projet = a.id_projet left join 
-          $progecen_vue_tps_a t on t.e_id_action = a.id_action::text
-	  WHERE p.id_projet = p2.id_projet
-    GROUP BY 1,2,3,4,5,6,9
-	  ORDER BY 3
-	) acts
-  )::text as json_actions
-  FROM $progecen_projets p
-)
-SELECT json_agg(t) FROM t
- */
+
+
+//p.etat <> 'Réalisé'  AND
 
 
 $result = pg_execute($dbconn, "sql", array($year_));
