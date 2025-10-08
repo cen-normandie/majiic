85a88864f1e0bb3c5036de043c5cb66e1661741a
Benno <b.perceval@superceval.fr>
2025-10-08 18:09:22 +0200

    Update test.php

--- test.php
+++ test.php
@@ -14,7 +14,6 @@ $path = $envRoot ?: __DIR__;
 require_once $path . '/vendor/autoload.php';
 
 $mpdf = new \Mpdf\Mpdf(['tempDir' => __DIR__ . '/download/pdf/']);
-$mpdf = new \Mpdf\Mpdf(['mode' => 'c']);
 
 $mpdf->mirrorMargins = true;
 
