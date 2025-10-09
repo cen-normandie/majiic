46eea0399105aefebd083bcdc015f120dd389a74
benno-p <b.perceval@superceval.fr>
2025-10-09 16:35:58 +0200

    Update export_h.js

--- js/export_h.js
+++ js/export_h.js
@@ -74,7 +74,7 @@ function load_temps_ajax () {
 
             $.ajax({
                 url      : "pdf.php",
-                data     : {tableau : $str__, name : 'export_horodate'},
+                data     : {tableau : $str__, name : 'export_horodate.pdf'},
                 method   : "POST",
                 dataType : "json",
                 async    : true,
