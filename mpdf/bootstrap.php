3eb1d73b90204b31090241980453447c52a3f390
benno-p <b.perceval@superceval.fr>
2025-10-09 12:58:13 +0200

    Update bootstrap.php

--- mpdf/bootstrap.php
+++ mpdf/bootstrap.php
@@ -1,10 +1,13 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // require composer autoload
-//$envRoot = getenv('MPDF_ROOT');
-//$path = $envRoot ?: __DIR__;
+$envRoot = getenv('MPDF_ROOT');
+$path = $envRoot ?: __DIR__;
 
-require_once '/var/www/html/majiic/vendor/autoload.php';
+require_once $path . '/vendor/autoload.php';
 
 Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT, __DIR__ . '/log');
 Tracy\Debugger::$strictMode = true;
