aed663812fc53ce68e02a1db31744407f2b58ee8
benno-p <b.perceval@superceval.fr>
2025-10-09 12:13:25 +0200

    Update bootstrap.php

--- mpdf/bootstrap.php
+++ mpdf/bootstrap.php
@@ -4,7 +4,7 @@
 $envRoot = getenv('MPDF_ROOT');
 $path = $envRoot ?: __DIR__;
 
-require_once $path . '/vendor/autoload.php';
+require_once $path . '/../vendor/autoload.php';
 
 Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT, __DIR__ . '/log');
 Tracy\Debugger::$strictMode = true;
